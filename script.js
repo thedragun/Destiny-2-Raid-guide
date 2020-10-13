4775874b2edf18fc3b4a58a19dcfd05831d82e6c
thedragun <62728488+thedragun@users.noreply.github.com>
2020-10-13 17:37:51 +1300

    please let this fix it

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
  }
 }
 
-window.onscroll = scrollFunction();
+document.body.onscroll = scrollFunction();
 
 function upTop() {document.documentElement.scrollTop = 0;}
+
+function element() {
+  document.getelementbyid("dropButton")
+}
+
+function dropMenu() {
+  element.classlist.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbutton')) {
+    var dropdowns = document.getElementsByClassName("dropMenu");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
